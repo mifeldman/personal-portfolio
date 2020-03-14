00e07d7575120234a302b1eff7ada0c87e98fa4c
Micah Feldman <micahdfeldman@gmail.com>
2020-03-14 02:00:47 -0600

    Added Starships and Began Debugging

--- scripts/uti.js
+++ scripts/uti.js
@@ -12,3 +12,26 @@ export function getLastNumber(url) {
       element.removeChild(element.firstChild);
     } 
   }
+
+  export function addStarField (element, numStars) {
+    element.style.setProperty('background-color', 'black')
+    for (let i = 0; i< numStars; i++) {
+    let star = document.createElement('div')
+    star.style.setProperty('position', 'absolute')
+    star.style.setProperty('width', '2px')
+    star.style.setProperty('width', '2px')
+    star.style.setProperty('background-color', 'white')
+    let xy = getRandomPosition()
+    star.style.left = `${xy[0]}px`
+    star.style.top = `${xy[1]}px`
+    element.appendChild
+    }
+  }
+
+  function getRandomPosition() {
+    let y = 5000
+    let x = 5000
+    let randomY = Math.floor(Math.random() * y)
+    let randomX = Math.floor(Math.random() * x)
+    return [randomX, randomY]
+  }
--- starships/main.js
+++ starships/main.js
@@ -1,8 +1,21 @@
 import { starships } from "../data/starships.js";
+import { getLastNumber, removeChildren, addStarField } from '../scripts/uti.js'
+
 
 const nav = document.querySelector('.nav')
-
 const navList = document.querySelector('.navList')
+const shipView = document.querySelector('.main')
+const dialog = document.querySelector('.modal')
+const closeButton = document.querySelector('.modal-close')
+const modalBackground = document.querySelector('.modal-background')
+
+closeButton.addEventListener('click', () => {
+  dialog.classList.toggle('is-active')
+})
+
+modalBackground.addEventListener('click', () => {
+  dialog.classList.toggle('is-active')
+})
 
 
 function populateNav(starships) {
@@ -10,7 +23,11 @@ function populateNav(starships) {
 
       let anchorWrap = document.createElement("a");
       anchorWrap.href = "#";
-
+      anchorWrap.addEventListener('click', event => {
+        let shipName = event.target.textContent
+        const foundShip = starships.find(ship => ship.name === shipName)
+        populateShipView(foundShip)
+      })
       let listItem = document.createElement('li')
       listItem.textContent = starship.name
 
@@ -20,6 +37,21 @@ function populateNav(starships) {
     });
   }
 
-  //look at the code after 12:20...that is when you got lost
+function populateShipView(shipData) {
+  removeChildren(shipView)
+  let shipNum = getLastNumber(shipData.url);
+  let shipImage = document.createElement("img");
+  shipImage.src = `https://starwars-visualguide.com/#/starships/${shipNum}.jpg`;
+  shipView.appendChild(shipImage)
+  imageItem.addEventListener("error", event => {
+    dialog.classList.toggle('is-active')
+    shipImage.hidden = true;
+  });
+  shipView.appendChild()
+
+}
+
 
   populateNav(starships);
+
+  addStarField(document.querySelector('body'), 1000)
