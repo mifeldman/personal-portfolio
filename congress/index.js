38b61204932a2bf369eb5c5eb6724afac2bd5dea
Micah Feldman <micahdfeldman@gmail.com>
2020-03-31 12:52:34 -0600

    Fixed Project Tiles & Started Congress Seniority

--- congress/index.js
+++ congress/index.js
@@ -4,24 +4,21 @@ const senatorDiv = document.querySelector('.senators');
 
 const main = document.querySelector('main');
 
-function populateSenatorDiv() {
-    senators.forEach(senator => {
-        let middleName = senator.middle_name ? ` ${senator.middle_name} ` : ` `
-
+function populateSenatorDiv(justSenatorNames) {
+    console.log(justSenatorNames)
+    justSenatorNames.forEach(senator => {
         let senFigure = document.createElement('figure')
         let figImg = document.createElement('img')
         let figCaption = document.createElement('figcaption')
     
-        figImg.src = `https://www.govtrack.us/static/legislator-photos/${senator.govtrack_id}-200px.jpeg`
-        
-        figCaption.textContent = `${senator.first_name}${middleName}${senator.last_name}`
+        figImg.src = senator.imgURL
+        figCaption.textContent = senator.name
 
         senFigure.appendChild(figImg)
         senFigure.appendChild(figCaption)
         senatorDiv.appendChild(senFigure)
 
     })
-    main.appendChild(senatorDiv)
 }
 
 const filterSenators = (prop, value) => {
@@ -30,9 +27,6 @@ const filterSenators = (prop, value) => {
     })
 }
 
-// console.log(filterSenators('party', 'R'))
-// console.log(filterSenators('party', 'D'))
-// console.log(filterSenators('party', 'ID'))
 
 const senatorNames = senators.map(senator => {
     let middleName = senator.middle_name ? ` ${senator.middle_name} ` : ` `
@@ -40,11 +34,9 @@ const senatorNames = senators.map(senator => {
         id: senator.id,
         name: `${senator.first_name}${middleName}${senator.last_name}`,
         imgURL: `https://www.govtrack.us/static/legislator-photos/${senator.govtrack_id}-200px.jpeg`
+        //seniority
     }
 })
 
-//https://www.govtrack.us/static/legislator-photos/300002-200px.jpeg
 
-console.log(senatorNames)
-
-populateSenatorDiv();
+populateSenatorDiv(senators);
