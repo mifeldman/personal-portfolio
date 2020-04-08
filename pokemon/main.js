543ec9f2235bf10d10ecd3a4d18fb5c653413b55
Micah Feldman <micahdfeldman@gmail.com>
2020-04-08 16:33:29 -0600

    Updated PokeJS to have Fetch

--- pokemon/main.js
+++ pokemon/main.js
@@ -1,16 +1,53 @@
+const allPokemon = []
+
 function getPokeData(url) {
-    let pokemonGrid = document.querySelector('.pokemonGrid')
-    fetch(url).then(function (response) {
-        response.json().then(function (data) {
-            console.log(data)
-            //pokemonGrid.textcontent = data.name
+  fetch(url).then(function (response){
+    response.json().then(function (pokeData) {
+      console.log(pokeData.results)
+      const pokeMap = pokeData.results.map (pokemon => {
+        return fetch (pokemon.url).then (resData => {
+          resData.json().then(pokeJson => {
+            allPokemon.push(pokeJson)
+          })
         })
+      })
     })
+  })
 }
 
-getPokeData('https://pokeapi.co/api/v2/pokemon/1/')
+let pokemonGrid = document.querySelector('.pokemonGrid')
 
-var card = document.querySelector('.card');
-card.addEventListener( 'click', function() {
-  card.classList.toggle('is-flipped');
-});
+getPokeData('https://pokeapi.co/api/v2/pokemon?&limit=25')
+
+console.log(allPokemon)
+
+populatePokeCards(allPokemon)
+
+
+function populatePokeCard(pokeArray) {
+  pokeArray.forEach((pokemon) => {
+    let pokeScene = document.createElement("div");
+    pokeScene.className = "scene";
+    let pokeCard = document.createElement("div");
+    pokeCard.className = "card";
+    pokeCard.addEventListener("click", () =>
+      pokeCard.classList.toggle("is-flipped")
+    );
+    let pokeFront = document.createElement("div");
+    pokeFront.className = "card__face card__face--front";
+    pokeFront.textContent = "front";
+    let pokeBack = document.createElement("div");
+    pokeBack.className = "card__face card__face--back";
+    pokeBack.textContent = "back";
+
+    pokeCard.appendChild(pokeFront);
+    pokeCard.appendChild(pokeBack);
+    pokeScene.appendChild(pokeCard);
+    pokemonGrid.appendChild(pokeScene);
+  });
+}
+
+// var card = document.querySelector(".card");
+// card.addEventListener("click", function () {
+//   card.classList.toggle("is-flipped");
+// });
